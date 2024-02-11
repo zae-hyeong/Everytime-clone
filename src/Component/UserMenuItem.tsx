3e2f20efad0af9e24c6ec9b8d8c1b3ec3be52223
zae-hyeong <p0138@naver.com>
2024-02-11 15:23:54 +0900

    [feat] add background-img in UserMenuItem

--- src/Component/UserMenuItem.tsx
+++ src/Component/UserMenuItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserMenuItem:React.FC<{linkExplain:string, bgImg: string}> = (props) => {
   return (
-    <a className={props.bgImg+'w-[40px] h-[40px] bg-red-500'} href='#'>{props.linkExplain}</a>
+    <a className={props.bgImg+' block w-[40px] h-[40px] bg-cover text-transparent rounded-md p-1 border bg-origin-content bg-no-repeat m-1'} href='#'>{props.linkExplain}</a>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'account': "url('/src/public/img/user-button.png')",
+        'chat': "url('/src/public/img/chat-button.png')"
+      },
+    },
   },
   plugins: [],
 };
