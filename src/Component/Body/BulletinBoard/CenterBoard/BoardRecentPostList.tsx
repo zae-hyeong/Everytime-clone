2e1c2120e1126f9c0266b550f3fd95ed195a41fa
zae-hyeong <p0138@naver.com>
2024-02-14 19:52:15 +0900

    [feat] add uploadBefore in PostList

--- src/Component/Body/BulletinBoard/CenterBoard/BoardRecentPostList.tsx
+++ src/Component/Body/BulletinBoard/CenterBoard/BoardRecentPostList.tsx
@@ -14,9 +14,12 @@ const BoardRecentPostList: React.FC<{
           </a>
         </li>
         {props.recentPosts.map((boardPost) => (
-          <li className="p-2 text-sm border border-t-0 hover:bg-gray-100"><a href="#">
-            {boardPost.postTitle}
-            </a></li>
+          <li className="p-2 text-sm border border-t-0 hover:bg-gray-100">
+            <a href="#" className="flex items-center justify-between">
+            <span className="grow-1 block whitespace-nowrap overflow-hidden text-ellipsis">{boardPost.postTitle}</span>
+            <span className="text-xs shrink-0 text-gray-400 pl-2">{boardPost.uploadBefore}</span>
+            </a>
+            </li>
         ))}
       </ul>
     </li>
--- src/Component/Body/BulletinBoard/CenterBoard/MainBoardArea.tsx
+++ src/Component/Body/BulletinBoard/CenterBoard/MainBoardArea.tsx
@@ -21,7 +21,7 @@ const dummyBoardTitle: string[] = [
 
 const dummyBoardPosts: PostSummery[] = [
   new PostSummery("드럼 vs 기타", "방금"),
-  new PostSummery("학자금 지원구간 질문", "방금"),
+  new PostSummery("학자금 지원구간 질문은 과사에다가 해야하는거냐?", "5분 전"),
   new PostSummery("일감호 런닝 뛸사람?", "02/13 15:49"),
   new PostSummery("기숙사 건조기 추가시간", "02/13 13:17"),
 ];
