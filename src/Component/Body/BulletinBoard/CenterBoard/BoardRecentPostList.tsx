9def851449de4f4b3869e85a7eae717dfb5da0c9
zae-hyeong <p0138@naver.com>
2024-02-14 20:07:42 +0900

    [factor] split BoardRecentPost from BoardRecentPostList

--- src/Component/Body/BulletinBoard/CenterBoard/BoardRecentPostList.tsx
+++ src/Component/Body/BulletinBoard/CenterBoard/BoardRecentPostList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostSummery from "../../../../public/class/PostSummery";
+import BoardRecentPost from "./BoardRecentPost";
 
 const BoardRecentPostList: React.FC<{
   postListTitle: string;
@@ -10,16 +11,13 @@ const BoardRecentPostList: React.FC<{
       <ul>
         <li>
           <a href="#">
-            <h3 className="text-main font-bold p-2 border">{props.postListTitle}</h3>
+            <h3 className="text-main font-bold p-2 border">
+              {props.postListTitle}
+            </h3>
           </a>
         </li>
         {props.recentPosts.map((boardPost) => (
-          <li className="p-2 text-sm border border-t-0 hover:bg-gray-100">
-            <a href="#" className="flex items-center justify-between">
-            <span className="grow-1 block whitespace-nowrap overflow-hidden text-ellipsis">{boardPost.postTitle}</span>
-            <span className="text-xs shrink-0 text-gray-400 pl-2">{boardPost.uploadBefore}</span>
-            </a>
-            </li>
+          <BoardRecentPost boardPost={boardPost} />
         ))}
       </ul>
     </li>
--- src/Component/Body/BulletinBoard/RightBoard/RepresentativePostArea.tsx
+++ src/Component/Body/BulletinBoard/RightBoard/RepresentativePostArea.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+const dummyRepPostTitles: string[] = [
+  '실시간 인기 글', 'HOT 게시물', 'BEST 게시판', '학교 소식', '최근 강의평'
+]
+
 const RepresentativePostArea:React.FC = () => {
   return (
-    <div className='bg-blue-400 row-span-2'>3</div>
+    <div className='bg-blue-400 row-span-2'>
+      <form action="submit">
+        <input type="text" placeholder='전체 게시판의 글을 검색하세요!' className='w-full border-2 p-2'/>
+      </form>
+      
+    </div>
   )
 }
 
