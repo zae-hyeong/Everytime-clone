911922ad920db503d46bdaa2739d7699cbc2b290
zae-hyeong <p0138@naver.com>
2024-03-06 21:38:11 +0900

    [feat] add attached image in PostMainBody

--- src/Component/Body/BulletinBoard/Post/PostMainContentAsset/PostMainBody.tsx
+++ src/Component/Body/BulletinBoard/Post/PostMainContentAsset/PostMainBody.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 export interface IPostMainBodyProps {
   title: string;
@@ -6,11 +6,14 @@ export interface IPostMainBodyProps {
   images?: string[];
 }
 
-export default function PostMainBody (props: IPostMainBodyProps) {
+export default function PostMainBody(props: IPostMainBodyProps) {
   return (
-    <>
-    <h1 className="text-2xl font-bold mb-3">{props.title}</h1>
+    <div className="w-full mb-4">
+      <h1 className="text-2xl font-bold mb-3">{props.title}</h1>
       <p className="mb-3">{props.content}</p>
-    </>
+      {props.images ? <div className="w-full">
+        {props.images.map(img => <img src={img} alt="post attached files" />)}
+      </div> : <></>}
+    </div>
   );
 }
