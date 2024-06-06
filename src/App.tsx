6e8d26fe16a09b06447ac6c0a578b9eb072cafa0
zae-hyeong <p0138@naver.com>
2024-06-06 15:50:04 +0900

    [optimize] use loader at post list in  BoarderList

--- src/App.tsx
+++ src/App.tsx
@@ -11,38 +11,47 @@ import GradeCalculator from "Component/pages/MenuPages/GradeCalculator/GradeCalc
 import Lectures from "Component/pages/MenuPages/Lectures/Lectures";
 import TimeTable from "Component/pages/MenuPages/TimeTabel/TimeTable";
 import RootLayout from "Component/pages/RootLayout";
+import { SERVER_URL } from "public/server";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <BulletinBoardLayout />,
-        children: [
-          { path: "/", element: <BulletinBoard /> }, 
-          {
-            path: "/Board/:boardId",
-            element: <BoardLayout />,
-            children: [
-              { path: "", element: <Board /> },
-              { path: ":postId", element: <Post /> }
-            ],
-          },
-        ],
-      },
-      { path: "/TimeTable", element: <TimeTable /> },
-      { path: "/Lectures", element: <Lectures /> },
-      { path: "/GradeCalculator", element: <GradeCalculator /> },
-      { path: "/Friends", element: <Friends /> },
-      { path: "/BookStore", element: <BookStore /> },
-      { path: "/CampusPick", element: <CampusPick /> },
-    ],
-  }
-], {basename: "/Everytime-clone",});
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <BulletinBoardLayout />,
+          children: [
+            { path: "/", element: <BulletinBoard /> },
+            {
+              id: "board-posts",
+              path: "/Board/:boardId",
+              element: <BoardLayout />,
+              loader: async () => {
+                const postsResponse = await fetch(SERVER_URL + "/posts");
+                return await postsResponse.json();
+              },
+              children: [
+                { path: "", element: <Board /> },
+                { path: ":postId", element: <Post /> },
+              ],
+            },
+          ],
+        },
+        { path: "/TimeTable", element: <TimeTable /> },
+        { path: "/Lectures", element: <Lectures /> },
+        { path: "/GradeCalculator", element: <GradeCalculator /> },
+        { path: "/Friends", element: <Friends /> },
+        { path: "/BookStore", element: <BookStore /> },
+        { path: "/CampusPick", element: <CampusPick /> },
+      ],
+    },
+  ],
+  { basename: "/Everytime-clone" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/Component/pages/BulletinBoard/Board/assets/BoardList.tsx
+++ src/Component/pages/BulletinBoard/Board/assets/BoardList.tsx
@@ -1,46 +1,30 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
-import { SERVER_URL } from "public/server";
 import { IPost } from "public/class/Post";
 import { RootState, useAppDispatch } from "Component/Redux/Store";
 import { setPosts } from "Component/Redux/postSlice";
 import BoardListItem from "./BoardListItem";
-import Loading from "Component/PublicAsset/Loading";
+import { useRouteLoaderData } from "react-router-dom";
 
 export interface IBoardListProps {}
 
 export default function BoardList(props: IBoardListProps) {
   const page = useSelector((state: RootState) => state.board.boardPage);
-  const posts = useSelector((state: RootState) => state.post.posts);
-
-  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const postsState = useSelector((state: RootState) => state.post.posts);
 
   const dispatch = useAppDispatch();
 
-  React.useEffect(() => {
-    async function fetchPosts() {
-      try {
-        setIsLoading(true);
-        const postsResponse = await fetch(SERVER_URL + "/posts");
-        const { posts } = await postsResponse.json();
-        setIsLoading(false)
-        dispatch(setPosts(posts));
-      } catch (e) {
-        alert("ERROR : " + e);
-      }
-    }
-    fetchPosts();
-  }, [dispatch]);
+  const { posts } = useRouteLoaderData("board-posts") as { posts: IPost[] };
+  dispatch(setPosts(posts));
 
   const startPage = page * 10;
   const endPage = page * 10 + 9;
 
-  return (<>
-    {isLoading ? <Loading /> : <ul>
-      {posts.slice(startPage, endPage).map((post: IPost) => (
+  return (
+    <ul>
+      {postsState.slice(startPage, endPage).map((post: IPost) => (
         <BoardListItem post={post} key={post.postId} />
       ))}
-    </ul>}
-    </>
+    </ul>
   );
 }
